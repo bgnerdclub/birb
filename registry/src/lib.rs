badfa62dc1b1dddbbe5222970087712201530d4d
cam <130924891+camsoftworks2018@users.noreply.github.com>
2023-12-11 17:49:19 +0000

    Implemented registry SET & GET - Non Persistent Implemented Unit Test

--- registry/src/lib.rs
+++ registry/src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![feature(downcast_unchecked)]
+#![feature(trait_upcasting)]
+#![feature(const_trait_impl)]
+
+use birb::Module;
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug)]
+pub struct Registry {
+    data: HashMap<String, Vec<u8>>
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+// Implement Registry Methods
+impl Registry {
+    pub fn store<T>(&mut self, key: String, object: &T) where T: Serialize {
+        let jstring = serde_json::to_string(object);
+        self.data.insert(key, jstring.unwrap().into_bytes());
     }
+    pub fn get<T>(&self, key: String) -> T where T: for<'a> Deserialize<'a> {
+        let jstring = self.data.get(&key).unwrap();
+        let object: T = serde_json::from_slice(jstring).unwrap();
+        return object
+    }
+}
+
+impl Module for Registry {}
+
+#[test]
+fn test_registry_kv() {
+    let mut registry = Registry { data: HashMap::new() };
+    const INDEX_VALUE: &str = "test_value";
+    const INDEX_KEY: &str = "TEST/key";
+    registry.store::<String>(INDEX_KEY.to_string(), &INDEX_VALUE.to_string());
+    let rsval = registry.get::<String>(INDEX_KEY.to_string());
+    assert_eq!(&rsval, &INDEX_VALUE);
 }
