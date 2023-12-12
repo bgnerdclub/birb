9e6e280157419516a7373add5278b4821ecf43e8
cam <130924891+camsoftworks2018@users.noreply.github.com>
2023-12-12 17:08:20 +0000

    finished file handling & crash security (it panics still until we have a unified logging handler)

--- registry/src/lib.rs
+++ registry/src/lib.rs
@@ -1,38 +1,118 @@
 #![feature(const_trait_impl)]
+#![feature(fs_try_exists)]
 
-use birb::Module;
-use std::collections::HashMap;
+use birb::MainThreadModule;
+use std::{collections::HashMap, io::prelude::*, fs::File, fs};
+use std::fmt::{Debug, Formatter};
 use serde::{Serialize, Deserialize};
 
-#[derive(Debug, Default)]
+#[derive(Default)]
 pub struct Registry {
+    ticks: u16,
     data: HashMap<String, Vec<u8>>
 }
 
 // Implement Registry Methods
 impl Registry {
     pub fn new() -> Self {
-        Self::default()
+        Registry::default()
     }
     pub fn store<T>(&mut self, key: String, object: &T) where T: Serialize {
-        let jstring = serde_json::to_string(object);
-        self.data.insert(key, jstring.unwrap().into_bytes());
+        self.data.insert(key, serde_json::to_string(object).unwrap().into_bytes());
     }
     pub fn get<T>(&self, key: String) -> T where T: for<'a> Deserialize<'a> {
-        let jstring = self.data.get(&key).unwrap();
-        let object: T = serde_json::from_slice(jstring).unwrap();
+        let object: T = serde_json::from_slice(self.data.get(&key).unwrap()).unwrap();
         return object
     }
 }
 
-impl Module for Registry {}
+impl Debug for Registry {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Registry")
+            .field("data", &self.data)
+            .finish()
+    }
+}
 
-#[test]
-fn test_registry_kv() {
-    let mut registry = Registry::new();
-    const INDEX_VALUE: &str = "test_value";
-    const INDEX_KEY: &str = "TEST/key";
-    registry.store::<String>(INDEX_KEY.to_string(), &INDEX_VALUE.to_string());
-    let rsval = registry.get::<String>(INDEX_KEY.to_string());
-    assert_eq!(&rsval, &INDEX_VALUE);
+impl MainThreadModule for Registry {
+    fn tick(&mut self, _: &birb::MainThreadApp) {
+        // shitty persistence :3
+        // loads json on zero
+        if self.ticks == 0 {
+            if fs::try_exists("registry.json").unwrap() {
+                match File::open("registry.json") {
+                    Ok(mut File) => {
+                        let mut data = String::new();
+                        match File.read_to_string(&mut data) {
+                            Ok(_) => {
+                                self.data = serde_json::from_str(&data).unwrap()
+                            }
+                            Err(E) => {
+                                fs::remove_file("registry.json").unwrap();
+                                self.ticks = 0;
+                                // early return to escape ++ increment on internal tick counter.
+                                return
+                            }
+                        }
+                    }
+                    Err(E) => {
+                        panic!("error in loading persistence \n {}", E);
+                    }
+                }
+            } else {
+                match File::create("registry.json") {
+                    Ok(mut File) => {
+                        // create from empty Hashmap Array
+                        match File.write(serde_json::to_vec(&self.data).unwrap().as_slice()) {
+                            Err(E) => {
+                                panic!("error saving persistence \n {}", E)
+                            }
+                            _ => {
+                                // discard, dont care about OK() result
+                            }
+                        }
+                    }
+                    Err (E) => {
+                        panic!("error in creating persistence \n {}", E);
+                    }
+                }
+            }
+        } else {
+            // every 5 seconds
+            if self.ticks > 300 {
+                match File::open("registry.json") {
+                    Ok(mut File) => {
+                        match File.write(serde_json::to_vec(&self.data).unwrap().as_slice()) {
+                            Err (E) => {
+                                panic!("error in persisting persistence \n {}", E);
+                            }
+                            _ => {
+                                // discard, dont care about OK() result
+                            }
+                        }
+                    }
+                    Err(E) => {
+                        panic!("error in persisting persistence \n {}", E);
+                    }
+                }
+                self.ticks = 0;
+            }
+        }
+        self.ticks += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Registry;
+
+    #[test]
+    fn test_registry_kv() {
+        let mut registry = Registry::new();
+        const INDEX_VALUE: &str = "test_value";
+        const INDEX_KEY: &str = "TEST/key";
+        registry.store::<String>(INDEX_KEY.to_string(), &INDEX_VALUE.to_string());
+        let return_value = registry.get::<String>(INDEX_KEY.to_string());
+        assert_eq!(&return_value, &INDEX_VALUE);
+    }
 }
