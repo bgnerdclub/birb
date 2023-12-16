928560fa80a6c36a37910f84e13631b4964b096b
Jake <65215994+mavic7@users.noreply.github.com>
2023-12-16 16:44:50 +0000

    Make clippy happy & cargo fmt

--- birb_log/src/lib.rs
+++ birb_log/src/lib.rs
@@ -1,40 +1,37 @@
 use birb::Module;
 use std::time::SystemTime;
 
-
-
 pub enum LogCategory {
     INFO,
     DEBUG,
     WARN,
-    ERROR
+    ERROR,
 }
 pub struct LogEntry {
     pub module: String,
     pub msg: String,
     pub timestamp: u64,
-    pub category: LogCategory
+    pub category: LogCategory,
 }
 
 pub type Listener = fn(&LogEntry);
 
-
-#[derive(Default,Debug)]
-pub struct Log{
-    listeners: Vec<Listener>
+#[derive(Default, Debug)]
+pub struct Log {
+    listeners: Vec<Listener>,
 }
 
 impl Log {
     #[must_use]
     pub fn new() -> Self {
-        return Self::default();
+        Self::default()
     }
 
-   fn translate_mod_uid<T>(&self,_object: &T) -> String {
-        return std::any::type_name::<T>().to_string();
+    fn translate_mod_uid<T>(&self, _object: &T) -> String {
+        std::any::type_name::<T>().to_string()
     } // temporary - will be changed as necessary later to reflect global module IDs, etc.
 
-   pub fn notify_listeners(&self, log: &LogEntry) {
+    pub fn notify_listeners(&self, log: &LogEntry) {
         for listener in &self.listeners {
             listener(log);
         }
@@ -48,16 +45,19 @@ impl Log {
         self.listeners.retain(|x| *x != func);
     }
     pub fn info<T>(&self, object: &T, message: String) {
-        let ts: u64 =  SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("SystemTime before unix epoch!").as_secs();
-        let module_name:String = self.translate_mod_uid(object);
+        let ts: u64 = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .expect("SystemTime before unix epoch!")
+            .as_secs();
+        let module_name: String = self.translate_mod_uid(object);
         let log_entry = LogEntry {
             module: module_name,
             msg: message,
             timestamp: ts,
-            category : LogCategory::INFO
+            category: LogCategory::INFO,
         };
         self.notify_listeners(&log_entry);
     }
 }
 
-impl Module for Log{}
+impl Module for Log {}
