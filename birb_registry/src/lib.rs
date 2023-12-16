c2667aaf5fc3d531b9de2fdd01d4b17426d9184d
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2023-12-16 15:40:19 +0000

    Remove default save path and make functions take T: AsRef<Path>

--- birb_registry/src/lib.rs
+++ birb_registry/src/lib.rs
@@ -1,47 +1,60 @@
-use birb::{Module};
-use std::{collections::HashMap, io::prelude::*, fs::File};
+use birb::Module;
+use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
-use std::path::{Path, PathBuf};
-use serde::{Serialize, Deserialize};
+use std::path::Path;
+use std::{collections::HashMap, fs::File, io::prelude::*};
 
 #[derive(Default, Debug)]
 pub struct Registry {
     data: HashMap<String, Vec<u8>>,
-    default_save_point: PathBuf
 }
-
-const SAVE_DEFAULT_LOCATION: &str = "registry_store.json";
 
 // Implement Registry Methods
 impl Registry {
     pub fn new() -> Self {
-        return Registry {
+        Registry {
             data: HashMap::default(),
-            default_save_point: PathBuf::from(SAVE_DEFAULT_LOCATION)
-        };
+        }
     }
-    pub fn store<T>(&mut self, key: String, object: &T) where T: Serialize {
-        self.data.insert(key, serde_json::to_string(object).unwrap().into_bytes());
+    pub fn store<T>(&mut self, key: String, object: &T)
+    where
+        T: Serialize,
+    {
+        self.data
+            .insert(key, serde_json::to_string(object).unwrap().into_bytes());
     }
-    pub fn get<T>(&self, key: String) -> T where T: for<'a> Deserialize<'a> {
+    pub fn get<T>(&self, key: String) -> T
+    where
+        T: for<'a> Deserialize<'a>,
+    {
         return serde_json::from_slice(self.data.get(&key).unwrap()).unwrap();
     }
-    pub fn load(&mut self, save_point: Option<&PathBuf>) {
-        if save_point.unwrap_or(&self.default_save_point).exists() {
-            let mut file = File::open(save_point.unwrap_or(&self.default_save_point)).unwrap();
+    pub fn load<T: AsRef<Path>>(&mut self, path: T) {
+        let path = path.as_ref();
+        if path.exists() {
+            let mut file = File::open(path).unwrap();
             let mut data = String::new();
             file.read_to_string(&mut data).unwrap();
             self.data = serde_json::from_str(&data).unwrap();
         }
     }
-    fn create_save_file(&mut self, save_point: Option<&PathBuf>) {
-        File::create(save_point.unwrap_or(&self.default_save_point)).unwrap().write(serde_json::to_vec(&self.data).unwrap().as_slice()).unwrap();
+
+    fn create_save_file<T: AsRef<Path>>(&mut self, path: T) {
+        File::create(path.as_ref())
+            .unwrap()
+            .write_all(serde_json::to_vec(&self.data).unwrap().as_slice())
+            .unwrap();
     }
-    pub fn save(&mut self, save_point: Option<&PathBuf>) {
-        if save_point.unwrap_or(&self.default_save_point).exists() {
-            File::open(save_point.unwrap_or(&self.default_save_point)).unwrap().write(serde_json::to_vec(&self.data).unwrap().as_slice()).unwrap();
+
+    pub fn save<T: AsRef<Path>>(&mut self, path: T) {
+        let path = path.as_ref();
+        if path.exists() {
+            File::open(path)
+                .unwrap()
+                .write_all(serde_json::to_vec(&self.data).unwrap().as_slice())
+                .unwrap();
         } else {
-            self.create_save_file(save_point)
+            self.create_save_file(path)
         }
     }
 }
@@ -61,3 +74,4 @@ mod tests {
         assert_eq!(&return_value, &INDEX_VALUE);
     }
 }
+
